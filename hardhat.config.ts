d5a03f6c45d447ccdd1de46add1803fdec337338
DoJun Lee <leedoe0709@gmail.com>
2024-02-17 11:08:06 +0900

    ENH: dotenv로 stage환경 설정

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import "dotenv/config";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   networks: {
@@ -11,6 +13,9 @@ const config: HardhatUserConfig = {
         "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
         "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a",
       ],
+    },
+    stage: {
+      url: process.env.STAGE_URL,
     },
   },
 };
